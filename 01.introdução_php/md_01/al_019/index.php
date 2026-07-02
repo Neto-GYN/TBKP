de07833b564eeca11cf9fe22d0f635c6025f6d3e
Neto Home <jsn.ads@gmail.com>
2026-07-01 21:36:03 -0300

    TBKP-INTRODUÇÃOPHP-MD01-AL019: introdução as condicionais

---
+++ 01.introdução_php/md_01/al_019/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// estruturas condicionais
+
+    if(condition){
+        "ação 1"
+    }
+
+    if (condition) {
+        "ação 1"
+    } else {
+        "ação 2"
+    }
+
+// operadores de comparação
+
+    // igual
+        == 
+    // diferente
+        !=
+    // (menor) (menor igual) (maior) (maior igual)
+        < <= > >=
+
+?>
+
