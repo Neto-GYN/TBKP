4b4a7699528b357552873e6796374d7e7854b2e3
Neto Home <jsn.ads@gmail.com>
2026-07-03 21:15:36 -0300

    TBKP-INTRODUÇÃOPHP-MD01-AL019: introdução as condicionais part 2

--- 01.introdução_php/md_01/al_019/index.php
+++ 01.introdução_php/md_01/al_019/index.php
@@ -21,5 +21,14 @@
     // (menor) (menor igual) (maior) (maior igual)
         < <= > >=
 
+// operadores logicos
+
+    // verifica se apenas uma codição e valida
+    ||
+    // todas codições tem quer valida
+    &&
+    // negação
+    ! 
+
 ?>
 
